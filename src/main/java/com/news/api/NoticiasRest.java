1b15da1170118d40b39962e86ba2d5800290493f
JesusCernaMedina <jcm.programing@gmail.com>
2021-10-23 00:59:25 -0500

    Se agrega etiqueta Cross

--- src/main/java/com/news/api/NoticiasRest.java
+++ src/main/java/com/news/api/NoticiasRest.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/news")
+@CrossOrigin
 public class NoticiasRest {
 
 	@Autowired
